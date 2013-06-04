615cc521c502b22409d0ad09aabc6f1973eac8ed
Seth Ladd <sethladd@google.com>
2013-06-04 02:46:23 -0700

    round trip

--- web/app.dart
+++ web/app.dart
@@ -1,3 +1,12 @@
-String hello = 'hello from dart';
+import 'package:web_ui/web_ui.dart';
+import 'dart:html';
+
+final List<String> catNames = toObservable([]);
+
+submitCat() {
+  var name = (query('#cat-name') as InputElement).value;
+  HttpRequest.request('/cats', method: 'POST', sendData: 'name=$name')
+      .then((req) => catNames.add(name));
+}
 
 main() {}
